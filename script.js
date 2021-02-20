60de6daff4e69f2da1eef8a641c398e4e65d9528
Pritam Kumar <77887792+pritamkr1@users.noreply.github.com>
2021-02-20 23:00:52 +0530

    some chnages

--- script.js
+++ script.js
@@ -8,27 +8,29 @@ var inputText = document.querySelector('#input-text');
 var outputText = document.querySelector('#output-text');
 var eventBtn = document.querySelector('.btn-primary');
 
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" ;
+var serverUrl = "https://api.funtranslations.com/translate/minion.json" ;
 
 function getTranslateUrl(text){
-    return serverUrl + "?" + "text=" + text;
+     return serverUrl + "?" + "text=" + text;
+    
 }
+
 eventBtn.addEventListener('click', doTranslate);
 
 function errorHandler(error){
-    alert("something wrong happen! please try agian later.")
+    alert("something wrong happen! please try agian later.", error);
 }
 
 function doTranslate(){
     var inputTxt = inputText.value;
-    console.log(inputTxt);
+  
 
     fetch(getTranslateUrl(inputTxt))
     .then(response => response.json())
     .then(json => {
-        var translatedText = json.contents.translated;
-        outputText.innerText = translatedText; 
+        var translatedTxt = json.contents.translated;
+        outputText.innerHTML = translatedTxt; 
           })
 
     .catch(errorHandler)
-}
+};
